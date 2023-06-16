2d1838b0488f5e72cd45464379840b6249df6586
Radosław Kowalski <ladekesl@gmail.com>
2023-06-16 19:16:08 +0200

    Created component user-profile with test content

---
+++ src/pages/UserProfile/user-profile.js
@@ -0,0 +1,6 @@
+
+export const UserProfile = () => {
+    return (
+       <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci aut blanditiis eum excepturi laborum qui similique sint ullam voluptatibus! Adipisci atque fugiat id mollitia optio quas, sint. Nam, <quia className="lorem">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium mollitia nam nulla quas rem? Accusantium, assumenda dicta doloribus ducimus eaque eos est hic inventore nesciunt odio odit veniam veritatis <voluptas className="lorem">HERE WILL BE PROFILE ACCOUNT</voluptas></quia></h1>
+    )
+}
