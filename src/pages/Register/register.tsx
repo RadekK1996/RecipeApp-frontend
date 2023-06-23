ccf115554c4490b847a117b6e445ee67049961b2
Radosław Kowalski <ladekesl@gmail.com>
2023-06-23 19:56:15 +0200

    Fixed import path

--- src/pages/Register/register.tsx
+++ src/pages/Register/register.tsx
@@ -1,4 +1,4 @@
-import {RegisterForm} from "../../components/FormAuth/RegisterForm.tsx";
+import {RegisterForm} from "../../components/FormAuth/RegisterForm";
 
 export const Register = () => {
     return (
